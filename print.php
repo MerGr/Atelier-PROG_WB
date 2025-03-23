17f5b7548c4b0b75d466a01826ed77808a5ac257
MerGr <fluffyspike3317@gmail.com>
2025-03-23 17:11:50 +0000

    fixed print issues

--- print.php
+++ print.php
@@ -1,74 +1,73 @@
 <?php
+ob_start();
 require_once('Classes.php');
 require_once('config.php');
 require_once('fpdf.php');
 session_start();
-class PDF extends FPDF {
 
-    function LoadData(){
+$index=intval($_GET['id'])+1;
+class PDF extends FPDF {
+    function LoadData($index){
         $conn=getConnection();
         if($conn){
-            $sql="SELECT * FROM etudiants";
-            $result=$conn->query($sql);
-            $data=[];
-            if($result->rowCount()>0){
-                while($row=$result->fetch(PDO::FETCH_ASSOC)){
-                    $data[]=new Etudiants($row['Nom'],$row['Maths'],$row['Informatique']);
-                }
-            }
+            $sql="SELECT Nom, Maths, Informatique FROM Notes WHERE ID=?";
+            $result=$conn->prepare($sql);
+            $result->execute([$index]);
+            $data=$result->fetch();
             closeConnection($conn);
             return $data;
         }
     }
 
     function Table($header, $data){
-        $this->SetFillColor(255,0,0);
-        $this->SetTextColor(255);
-        $this->SetDrawColor(128,0,0);
         $this->SetLineWidth(.3);
-        $this->SetFont('','B');
-        $w = array(40, 35, 40, 45);
-        for($i=0;$i<count($header);$i++)
-            $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
-        $this->Ln();
-        // Color and font restoration
-        $this->SetFillColor(224,235,255);
-        $this->SetTextColor(0);
+        $w = array(150,30);
+        
+        // Nom
+        $this->SetFillColor(220);
+        $this->SetFont('','B', 12);
+        $this->Cell($w[0],12,$header[0],0,0,'L',1);
         $this->SetFont('');
-        // Data
-        $fill = false;
-        foreach($data as $row)
-        {
-            $this->Cell($w[0],6,$row[1],'LR',0,'L',$fill);
-            $this->Cell($w[1],6,number_format($row[2]),'LR',0,'L',$fill);
-            $this->Cell($w[2],6,number_format($row[3]),'LR',0,'R',$fill);
-            $this->Cell($w[3],6,number_format(($row[2]+$row[3])/2),'LR',0,'R',$fill);
-            $this->Ln();
-            $fill = !$fill;
-        }
-        // Closing line
-        $this->Cell(array_sum($w),0,'','T');
+        $this->Cell($w[1],12,$data['Nom'],0,0,'R',1);
+        $this->Ln(30);
+        // Maths
+        $this->SetFont('','B');
+        $this->Cell($w[0],12,$header[1],'TBL',0,'L');
+        $this->SetFont('');
+        $this->Cell($w[1],12,number_format($data['Maths']),1,0,'R');
+        $this->Ln();
+        // Informatique
+        $this->SetFont('','B');
+        $this->Cell($w[0],12,$header[2],'TBL',0,'L',);
+        $this->SetFont('');
+        $this->Cell($w[1],12,number_format($data['Informatique']),1,0,'R');
+        $this->Ln();
+        // Moyenne
+        $moy=number_format(($data['Maths']+$data['Informatique'])/2);
+
+        $this->SetFont('','B');
+        $this->Cell($w[0],12,$header[3],'TBL',0,'L',1);
+        $this->Cell($w[1],12,$moy,1,0,'R',1);
+        $this->Ln(35);
+        // Observation
+        $obsrv=($moy >= 10.00) ? "ADMIS" : "NON ADMIS";
+
+        $this->Cell($w[0],12,$header[4],'TBL',0,'L',1);
+        $this->Cell($w[1],12, $obsrv,1,0,'R',1);
+        $this->Ln();
     }
     function Header(){
-        $this->Image('./assets/logo.png',10,6,30);
-        $this->SetFont('Arial','B',12);
-        $this->Cell(0,10,'Liste des etudiants',0,1,'C');
+        $this->Image('./assets/logo.png',10,6,40);
+        $this->SetFont('Arial','B',24);
+        $this->Cell(0,30,'Rapport',0,1,'C');
         $this->Ln(10);
     }
-
-    function Footer(){
-        $this->SetY(-15);
-        $this->SetFont('Arial','I',8);
-        $this->Cell(0,10,''.$this->PageNo().'/{nb}',0,0,'C');
-    }
 }
-
 $pdf = new PDF();
-$header = array('Nom', 'Maths', 'Informatique', 'Moyenne');
-$data = $pdf->LoadData();
-$pdf->AliasNbPages();
+$header = array('Nom', 'Maths', 'Informatique', 'Moyenne', 'Observation');
+$data = $pdf->LoadData($index);
 $pdf->AddPage();
 $pdf->Table($header, $data);
 $pdf->Output();
-
+ob_end_flush(); 
 ?>
